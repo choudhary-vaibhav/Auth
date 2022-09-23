79b264e190b401c71ea398749cad90b0ae6d440a
Vaibhav Choudhary <vaibhavch29@gmail.com>
2022-09-23 14:57:42 +0530

    completed registration & login

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,49 @@
 const repo = require('../db/repository/userOperations');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
     register(request, response){
+        const userObject = request.body;
+        console.log(userObject);
+        
+        const saltRounds = 10;
+        bcrypt.genSalt(saltRounds, (err, salt)=>{
+            if(err){
+                response.status(403).json({message:'Error in user registration! '});
+            }else{
+                bcrypt.hash(userObject.password, salt, (err, hash)=>{
+                    if(err){
+                        response.status(403).json({message:'Error in user registration! '});
+                    }else{
+                        userObject.password = hash;
+                        repo.add(userObject, response);
+                    }
+                })
+            }
+        })
 
     },
 
-    login(request, response){
-        
+    async login(request, response){
+        const userObject = request.body;
+        console.log(userObject);
+
+        const doc = await repo.find(userObject.email);
+        try{
+            if(doc && doc.email){
+                bcrypt.compare(userObject.password, doc.password, (err, result)=>{
+                    if(result){
+                        response.status(200).json({message:'userObject: ', doc});
+                    }else{
+                        response.status(404).json({message:'Invalid Password! '});
+                    }
+                })
+            }else{
+                response.status(404).json({message:'Invalid Email Provided! '});
+            }
+        }catch(err){
+            response.status(500).json({message:'Some DB Error! ', err});
+        }
     }
 }
--- db/repository/userOperations.js
+++ db/repository/userOperations.js
@@ -2,11 +2,20 @@ const UserModel = require('../models/user');
 
 module.exports = {
 
-    add(){
-
+    add(userObject, response){
+        UserModel.create(userObject, (err, doc)=>{
+            if(err){
+                response.status(500).json({message:'Some DB Error! ', err});
+            }else if(doc){
+                response.status(200).json({message:'User successfully registered! '});
+            }else{
+                response.status(403).json({message:'Error in user registration! '});
+            }
+        })
     },
 
-    find(){
-
+    find(userEmail){
+        const promise = UserModel.findOne({email:userEmail}).exec();
+        return promise;
     }
 }
